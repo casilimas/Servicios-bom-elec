99588cf838cc1097b8f15107314a0016befec489
casilimas <freddycasilimas@gmail.com>
2026-01-08 15:24:27 -0400

    nuevo fix

--- servicios-bomelec/src/App.jsx
+++ servicios-bomelec/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <p>Servicios eléctricos</p>
 
       <img
-        src="/img bomelec.jpg"
+        src="/img-bomelec.jpg"
         alt="BOME-ELEC"
         style={{
           width: '100%',
