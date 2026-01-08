a5cd7e88be5f15aeeedd9e7ad2f3c2327bfea4b7
casilimas <freddycasilimas@gmail.com>
2026-01-08 15:04:28 -0400

    deploy inicial de Bom-elec

---
+++ servicios-bomelec/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>BOME-ELEC</h1>
+      <p>Servicios eléctricos</p>
+
+      <img
+        src="/img bomelec.jpg"
+        alt="BOME-ELEC"
+        style={{
+          width: '100%',
+          maxHeight: '400px',
+          objectFit: 'cover',
+          borderRadius: '8px',
+          marginTop: '1rem'
+        }}
+      />
+    </>
+  )
+}
+
+export default App
